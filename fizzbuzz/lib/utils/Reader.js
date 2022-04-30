58614f989a90a85f6f1c01a565a2217cba1e1ec4
Jose Luis <launchx06519@innovaccion.mx>
2022-04-30 00:00:59 -0500

    4. Agregando Reader.js

---
+++ fizzbuzz/lib/utils/Reader.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+class Reader{
+
+    static readJsonFile(filePath){
+        const rawdata = fs.readFileSync(filePath);
+        return JSON.parse(rawdata); 
+    }
+
+}
+//const explorers = Reader.readJsonFile('explorers.json')
+//console.log(explorers)
+module.exports = Reader
