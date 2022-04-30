1ae75b81b9c9d4ad830e1925e6b34e6f3466b0cf
Jose Luis <launchx06519@innovaccion.mx>
2022-04-30 14:41:05 -0500

    4.Haciendo prueba unitaria del requerimiento 1: Filtrar explorers por mission

---
+++ test/services/ExplorerController.test.js
@@ -0,0 +1,79 @@
+const ExplorerController = require('./../../lib/controllers/ExplorerController.js')
+
+describe('Test ExplorerController',()=>{
+    test('Requerimiento 1: Filtrar la lista de explorer mission node',()=>{
+        const explorer1 = ExplorerController.getExplorersByMission('node')
+        expect(explorer1).toMatchObject([
+            {
+              name: 'Woopa1',
+              githubUsername: 'ajolonauta1',     
+              score: 1,
+              mission: 'node',
+              stacks: [ 'javascript', 'reasonML', 'elm' ]
+            },
+            {
+              name: 'Woopa2',
+              githubUsername: 'ajolonauta2',     
+              score: 2,
+              mission: 'node',
+              stacks: [ 'javascript', 'groovy', 'elm' ]
+            },
+            {
+              name: 'Woopa3',
+              githubUsername: 'ajolonauta3',     
+              score: 3,
+              mission: 'node',
+              stacks: [ 'elixir', 'groovy', 'reasonML' ]
+            },
+            {
+              name: 'Woopa4',
+              githubUsername: 'ajolonauta4',     
+              mission: 'node',
+              score: 4,
+              stacks: [ 'javascript' ]
+            },
+            {
+              name: 'Woopa5',
+              githubUsername: 'ajolonauta5',     
+              score: 5,
+              mission: 'node',
+              stacks: [ 'javascript', 'elixir', 'elm' ]
+            },
+            {
+              name: 'Woopa11',
+              githubUsername: 'ajolonauta11',    
+              score: 11,
+              mission: 'node',
+              stacks: [ 'javascript', 'elixir', 'groovy', 'reasonML', 'elm' ]
+            },
+            {
+              name: 'Woopa12',
+              githubUsername: 'ajolonauta12',    
+              score: 12,
+              mission: 'node',
+              stacks: [ 'javascript', 'elixir', 'groovy', 'reasonML', 'elm' ]
+            },
+            {
+              name: 'Woopa13',
+              githubUsername: 'ajolonauta13',    
+              score: 13,
+              mission: 'node',
+              stacks: [ 'javascript', 'elixir', 'groovy', 'reasonML', 'elm' ]
+            },
+            {
+              name: 'Woopa14',
+              githubUsername: 'ajolonauta14',    
+              score: 14,
+              mission: 'node',
+              stacks: [ 'javascript', 'elixir', 'groovy', 'reasonML', 'elm' ]
+            },
+            {
+              name: 'Woopa15',
+              githubUsername: 'ajolonauta15',    
+              score: 15,
+              mission: 'node',
+              stacks: [ 'javascript', 'elixir', 'groovy', 'reasonML', 'elm' ]
+            }
+          ])
+    })
+})
