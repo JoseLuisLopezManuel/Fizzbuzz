db1f8942f8117853fd7ff6a43bb4551b035f603a
Jose Luis <launchx06519@innovaccion.mx>
2022-04-30 01:16:24 -0500

    Agregando ESLint y ajustando archivos con Eslit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const explorers = JSON.parse(rawdata); //lista de explorers del archivo .json
 
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-console.log(explorersInNode.length)
+console.log(explorersInNode.length);
 
 // Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
@@ -33,7 +33,7 @@ const assignFizzTrick = function(explorer){
 };
 
 const explorersInNodeAndFizzTrick = explorersInNode.map((explorer) => assignFizzTrick(explorer));
-//console.log(explorersInNodeAndFizzTrick)
+console.log(explorersInNodeAndFizzTrick);
 
 // Part 6: Get a new list of explorers in node if the score number is divisible by 5, we need to set a new property called trick and set the value BUZZ, if not this value should be just the score
 //
@@ -48,7 +48,7 @@ const assignBuzzTrick = function(explorer){
 };
 
 const explorersInNodeAndBuzzTrick = explorersInNode.map((explorer) => assignBuzzTrick(explorer));
-//console.log(explorersInNodeAndBuzzTrick)
+console.log(explorersInNodeAndBuzzTrick);
 
 //Part7: Get a new list of explorers in Node, if the score number is divisible by 3 AND by 5 we need to set a new property called FIZZBUZZ, if not this value should be the same score value
 
@@ -63,6 +63,6 @@ const assignFizzBuzzTrick = function(explorer){
 };
 
 const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
-//console.log(explorersInNodeAndFizzBuzzTrick)
+console.log(explorersInNodeAndFizzBuzzTrick);
 
 // Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
